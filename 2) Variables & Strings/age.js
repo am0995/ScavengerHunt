852474214201387ddbebd38dcc52e458cce72a2c
am0995 <alyssam0995@tywls.org>
2016-10-05 22:05:02 +0000

    at&t scavenger hunt

---
+++ 2) Variables & Strings/age.js
@@ -0,0 +1,6 @@
+var age = prompt("What is your age?");
+
+var name = prompt("What's your name???")
+
+alert("Your name is " + name + " and your age is " + age);
+
---
+++ 3) Conditionals/scriptEdLove.js
@@ -0,0 +1,7 @@
+var love = prompt("Do you love ScriptEd? Yes or No?");
+
+if love == "Yes"{
+    alert("Yay!");
+}else if love == "No"{
+    alert("Boo!");
+}
